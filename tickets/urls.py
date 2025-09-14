cab426dfa9551233259ff68efe94cae6f7416d8b
Mohammed Ali Ahmed Jaber <mohdaaj.2000@gmail.com>
2025-09-14 09:36:04 +0300

    urls.py

---
+++ tickets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'tickets'
+
+urlpatterns = [
+    path('', views.ticket_list, name='ticket_list'),
+    path('create/', views.ticket_create, name='ticket_create'),
+    path('<int:pk>/', views.ticket_detail, name='ticket_detail'),
+    path('<int:pk>/edit/', views.ticket_update, name='ticket_update'),
+    path('<int:pk>/delete/', views.ticket_delete, name='ticket_delete'),
+]
