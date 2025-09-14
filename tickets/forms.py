09a3d0bd89a46a7e05cfa1b5677944d3339c4f78
Mohammed Ali Ahmed Jaber <mohdaaj.2000@gmail.com>
2025-09-14 09:12:23 +0300

    Form.py

---
+++ tickets/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Ticket, Comment
+
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'category']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
